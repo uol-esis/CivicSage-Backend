2dd3ec33cf597cf59304903bf22228c7b9d83bc3
Tilman Holube <tilman@holube.de>
2025-06-04 14:43:33 +0200

    Initial Commit

---
+++ src/main/java/de/uol/pgdoener/civicsage/CivicSageApplication.java
@@ -0,0 +1,13 @@
+package de.uol.pgdoener.civicsage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicSageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CivicSageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/uol/pgdoener/civicsage/CivicSageApplicationTests.java
@@ -0,0 +1,13 @@
+package de.uol.pgdoener.civicsage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicSageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
