298f026c71f8bb0d7dc9877f19bcdc45d3b72ae6
Tilman Holube <tilman@holube.de>
2025-06-05 18:57:40 +0200

    Implement indexing of files and logging search results

--- src/main/java/de/uol/pgdoener/civicsage/index/core/Chunk.java
+++
@@ -1,6 +0,0 @@
-package de.uol.pgdoener.civicsage.index.core;
-
-public sealed interface Chunk permits
-        PDFChunk,
-        PlainTextChunk {
-}
--- src/main/java/de/uol/pgdoener/civicsage/index/core/PlainTextChunk.java
+++
@@ -1,9 +0,0 @@
-package de.uol.pgdoener.civicsage.index.core;
-
-import lombok.NonNull;
-
-public record PlainTextChunk(
-        @NonNull String content,
-        int line
-) implements Chunk {
-}
