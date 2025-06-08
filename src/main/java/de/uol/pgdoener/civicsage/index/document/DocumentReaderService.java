a364830d7fb3ba683c2cc6139765c64034ddeb3f
Tilman Holube <tilman@holube.de>
2025-06-08 23:43:20 +0200

    add url to metadata of website document

--- src/main/java/de/uol/pgdoener/civicsage/index/document/DocumentReaderService.java
+++ src/main/java/de/uol/pgdoener/civicsage/index/document/DocumentReaderService.java
@@ -1,16 +1,22 @@
 package de.uol.pgdoener.civicsage.index.document;
 
 import de.uol.pgdoener.civicsage.index.exception.ReadFileException;
+import de.uol.pgdoener.civicsage.index.exception.ReadUrlException;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.document.DocumentReader;
 import org.springframework.ai.reader.jsoup.JsoupDocumentReader;
+import org.springframework.ai.reader.jsoup.config.JsoupDocumentReaderConfig;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+import static de.uol.pgdoener.civicsage.index.document.MetadataKeys.URL;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DocumentReaderService {
@@ -32,13 +38,16 @@ public class DocumentReaderService {
         if (url.matches("^[a-z]+://.+")) {
             //noinspection HttpUrlsUsage
             if (!(url.startsWith("http://") || url.startsWith("https://"))) {
-                throw new ReadFileException("Invalid protocol used in URL: " + url);
+                throw new ReadUrlException("Invalid protocol used in URL: " + url);
             }
         } else {
             url = "https://" + url;
         }
 
-        DocumentReader documentReader = new JsoupDocumentReader(url);
+        JsoupDocumentReaderConfig config = JsoupDocumentReaderConfig.builder()
+                .additionalMetadata(URL, url)
+                .build();
+        DocumentReader documentReader = new JsoupDocumentReader(url, config);
         return documentReader.read();
     }
 
