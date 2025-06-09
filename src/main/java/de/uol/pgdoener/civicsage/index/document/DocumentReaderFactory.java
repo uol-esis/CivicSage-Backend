4ed05b7ed18a52346fa5f87981e5115ebbe1202c
Tilman Holube <tilman@holube.de>
2025-06-09 18:36:09 +0200

    12 read word files (#21)
    * replace PlainTextDocumentReader with Spring AI implementation
    
    * Adding TikaDocumentReader
    
    * switch to reading files with the correct method

--- src/main/java/de/uol/pgdoener/civicsage/index/document/DocumentReaderFactory.java
+++ src/main/java/de/uol/pgdoener/civicsage/index/document/DocumentReaderFactory.java
@@ -1,15 +1,21 @@
 package de.uol.pgdoener.civicsage.index.document;
 
-import de.uol.pgdoener.civicsage.index.document.readers.PlainTextDocumentReader;
 import de.uol.pgdoener.civicsage.index.exception.ReadFileException;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.document.DocumentReader;
+import org.springframework.ai.reader.TextReader;
 import org.springframework.ai.reader.markdown.MarkdownDocumentReader;
 import org.springframework.ai.reader.markdown.config.MarkdownDocumentReaderConfig;
 import org.springframework.ai.reader.pdf.ParagraphPdfDocumentReader;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+import static de.uol.pgdoener.civicsage.index.document.MetadataKeys.FILE_NAME;
 
 @Slf4j
 @Component
@@ -19,16 +25,34 @@ public class DocumentReaderFactory {
         return switch (fileEnding) {
             case "txt" -> {
                 log.info("Indexing file as plain text file");
-                yield new PlainTextDocumentReader(file);
+                TextReader reader = new TextReader(file.getResource());
+                reader.getCustomMetadata().put(FILE_NAME, file.getOriginalFilename());
+                yield reader;
             }
             case "md", "markdown" -> {
                 log.info("Indexing file as Markdown file");
                 MarkdownDocumentReaderConfig config = MarkdownDocumentReaderConfig.defaultConfig();
+                config.additionalMetadata.put(FILE_NAME, file.getOriginalFilename());
                 yield new MarkdownDocumentReader(file.getResource(), config);
             }
             case "pdf" -> {
                 log.info("Indexing file as PDF file");
                 yield new ParagraphPdfDocumentReader(file.getResource());
+            }
+            // https://tika.apache.org/3.1.0/formats.html
+            case "odt", "odp", "ods", // LibreOffice
+                 "doc", "docx", "pptx", "xls", "xlsx" // Microsoft Office
+                    -> {
+                log.info("Indexing file with Tika");
+                yield new TikaDocumentReader(file.getResource()) {
+                    @NonNull
+                    @Override
+                    public List<Document> read() {
+                        return super.read().stream()
+                                .peek(d -> d.getMetadata().put(FILE_NAME, file.getOriginalFilename()))
+                                .toList();
+                    }
+                };
             }
             default -> throw new ReadFileException("Unsupported file type: " + fileEnding);
         };
