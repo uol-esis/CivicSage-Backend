ebb8d67aaf761f7e327674242d65c1ea6c7925a6
Tilman Holube <tilman@holube.de>
2025-06-18 23:54:48 +0200

    31 return more search results (#39)
    * implementing pagination and setting topK to return the required amount of results
    
    * fixing out of bounds requests if not enough results are available
    
    * Update src/main/java/de/uol/pgdoener/civicsage/search/SearchService.java
    
    Co-authored-by: Jonas Pohl <jonas.jp4c@gmail.com>
    
    ---------
    
    Co-authored-by: Jonas Pohl <jonas.jp4c@gmail.com>

--- src/main/java/de/uol/pgdoener/civicsage/search/SearchService.java
+++ src/main/java/de/uol/pgdoener/civicsage/search/SearchService.java
@@ -3,27 +3,38 @@ package de.uol.pgdoener.civicsage.search;
 import de.uol.pgdoener.civicsage.business.dto.SearchQueryDto;
 import de.uol.pgdoener.civicsage.business.dto.SearchResultDto;
 import de.uol.pgdoener.civicsage.mapper.SearchResultMapper;
+import de.uol.pgdoener.civicsage.search.exception.NotEnoughResultsAvailableException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SearchService {
 
+    private static final int DEFAULT_PAGE_NUMBER = 0;
+    private static final int DEFAULT_PAGE_SIZE = 20;
+
     private final VectorStore vectorStore;
     private final SearchResultMapper searchResultMapper;
 
-    public List<SearchResultDto> search(SearchQueryDto query) {
+    public List<SearchResultDto> search(SearchQueryDto query, Optional<Integer> pageNumber, Optional<Integer> pageSize) {
         log.info("Searching for documents with query {}", query);
+        int pNumber = pageNumber.orElse(DEFAULT_PAGE_NUMBER);
+        int pSize = pageSize.orElse(DEFAULT_PAGE_SIZE);
+        int resultsToFetch = (pNumber + 1) * pSize;
+        log.debug("topK = {}", resultsToFetch);
 
         // use SearchRequest instead of String to allow for more complex queries
-        List<Document> documents = vectorStore.similaritySearch(query.getQuery());
+        SearchRequest searchRequest = buildSearchRequest(query, resultsToFetch);
+        List<Document> documents = vectorStore.similaritySearch(searchRequest);
 
         if (documents == null || documents.isEmpty()) {
             log.info("No documents found for query {}", query);
@@ -31,11 +42,26 @@ public class SearchService {
         }
 
         log.info("Found {} documents for query {}", documents.size(), query);
-        for (Document document : documents) {
-            log.debug("Document: {}", document);
-        }
+        documents = applyPagination(documents, pNumber, pSize);
 
         return searchResultMapper.toDto(documents);
+    }
+
+    private SearchRequest buildSearchRequest(SearchQueryDto query, int resultsToFetch) {
+        return SearchRequest.builder()
+                .query(query.getQuery())
+                .topK(resultsToFetch)
+                .build();
+    }
+
+    private List<Document> applyPagination(List<Document> documents, int pageNumber, int pageSize) {
+        int startIndex = ((pageNumber + 1) * pageSize) - pageSize;
+        log.debug("startIndex = {}", startIndex);
+
+        if (startIndex >= documents.size())
+            throw new NotEnoughResultsAvailableException("Only " + documents.size() + " results were found!");
+
+        return documents.subList(startIndex, documents.size());
     }
 
 }
