40f308007b0254ae6b455b3c2287688b6244077b
Tilman Holube <tilman@holube.de>
2025-06-20 13:51:18 +0200

    remove unnecessary interface

--- src/main/java/de/uol/pgdoener/civicsage/source/FileSourceRepository.java
+++ src/main/java/de/uol/pgdoener/civicsage/source/FileSourceRepository.java
@@ -1,6 +1,5 @@
 package de.uol.pgdoener.civicsage.source;
 
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +7,6 @@ import java.util.UUID;
 
 @Repository
 public interface FileSourceRepository
-        extends CrudRepository<FileSource, UUID>,
-        JpaSpecificationExecutor<FileSource> {
+        extends CrudRepository<FileSource, UUID> {
 
 }
