80526dc03180988e1ca04fac5c9515973c3bd698
Jonas Pohl <jonas.jp4c@gmail.com>
2025-06-29 23:44:10 +0200

    Updating properties names.

--- src/main/java/de/uol/pgdoener/civicsage/CivicSageApplication.java
+++ src/main/java/de/uol/pgdoener/civicsage/CivicSageApplication.java
@@ -3,10 +3,8 @@ package de.uol.pgdoener.civicsage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 @ConfigurationPropertiesScan
 public class CivicSageApplication {
 
