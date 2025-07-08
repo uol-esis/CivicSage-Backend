19379e30353a0decf448ea49efc6d81786a1c11a
Tilman Holube <tilman@holube.de>
2025-07-08 19:16:00 +0200

    Wrap data in new resources for each document reader
    If the ParagraphPdfDocumentReader fails, the resource has to be read again. Since it is based on an InputStream, this does not work. Now the data is read in advance and wrapped in a ByteArrayResource for each DocumentReader.

--- src/main/java/de/uol/pgdoener/civicsage/index/document/reader/PdfDocumentReader.java
+++ src/main/java/de/uol/pgdoener/civicsage/index/document/reader/PdfDocumentReader.java
@@ -1,12 +1,15 @@
 package de.uol.pgdoener.civicsage.index.document.reader;
 
+import de.uol.pgdoener.civicsage.index.exception.ReadFileException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.document.DocumentReader;
 import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
 import org.springframework.ai.reader.pdf.ParagraphPdfDocumentReader;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 
+import java.io.IOException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -16,13 +19,29 @@ public class PdfDocumentReader implements DocumentReader {
 
     @Override
     public List<Document> get() {
+        byte[] data;
         try {
-            ParagraphPdfDocumentReader reader = new ParagraphPdfDocumentReader(resource);
+            data = resource.getContentAsByteArray();
+        } catch (IOException e) {
+            throw new ReadFileException("Could not read PDF file: " + resource.getFilename(), e);
+        }
+
+        try {
+            ParagraphPdfDocumentReader reader = new ParagraphPdfDocumentReader(constructRessourceFromData(data));
             return reader.read();
         } catch (RuntimeException e) {
-            PagePdfDocumentReader reader = new PagePdfDocumentReader(resource);
+            PagePdfDocumentReader reader = new PagePdfDocumentReader(constructRessourceFromData(data));
             return reader.read();
         }
+    }
+
+    private Resource constructRessourceFromData(byte[] data) {
+        return new ByteArrayResource(data) {
+            @Override
+            public String getFilename() {
+                return resource.getFilename();
+            }
+        };
     }
 
 }
