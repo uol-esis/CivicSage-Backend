da2e99f18f8f20190007a32409c6fbcf0d319f1d
Tilman Holube <tilman@holube.de>
2025-08-26 10:20:30 +0200

    Set user agent when indexing websites

--- src/main/java/de/uol/pgdoener/civicsage/business/index/document/reader/WebsiteDocumentReader.java
+++ src/main/java/de/uol/pgdoener/civicsage/business/index/document/reader/WebsiteDocumentReader.java
@@ -1,12 +1,18 @@
 package de.uol.pgdoener.civicsage.business.index.document.reader;
 
+import de.uol.pgdoener.civicsage.business.index.exception.ReadUrlException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.document.DocumentReader;
 import org.springframework.ai.reader.jsoup.JsoupDocumentReader;
 import org.springframework.ai.reader.jsoup.config.JsoupDocumentReaderConfig;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 
+import java.io.IOException;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,21 +28,41 @@ public class WebsiteDocumentReader implements DocumentReader {
 
     @Override
     public List<Document> get() {
-        JsoupDocumentReaderConfig config = JsoupDocumentReaderConfig.builder()
-                .additionalMetadata(additionalMetadata)
-                .selector(MAIN_SELECTOR)
-                .build();
-        List<Document> documents = new JsoupDocumentReader(url, config).read();
+        try {
+            Resource resource = getResource(url);
+            JsoupDocumentReaderConfig config = JsoupDocumentReaderConfig.builder()
+                    .additionalMetadata(additionalMetadata)
+                    .selector(MAIN_SELECTOR)
+                    .build();
+            List<Document> documents = new JsoupDocumentReader(resource, config).read();
 
-        if (areValid(documents)) {
-            return documents;
+            if (areValid(documents)) {
+                return documents;
+            }
+        } catch (IOException e) {
+            throw new ReadUrlException("Could not read website: " + url, e);
         }
 
-        // Try again without the main selector
-        config = JsoupDocumentReaderConfig.builder()
-                .additionalMetadata(additionalMetadata)
-                .build();
-        return new JsoupDocumentReader(url, config).read();
+        try {
+            Resource resource = getResource(url);
+            // Try again without the main selector
+            JsoupDocumentReaderConfig config = JsoupDocumentReaderConfig.builder()
+                    .additionalMetadata(additionalMetadata)
+                    .build();
+            return new JsoupDocumentReader(resource, config).read();
+        } catch (IOException e) {
+            throw new ReadUrlException("Could not read website: " + url, e);
+        }
+    }
+
+    private Resource getResource(String url) throws IOException {
+        return new UrlResource(url) {
+            @Override
+            protected void customizeConnection(@NotNull URLConnection con) throws IOException {
+                super.customizeConnection(con);
+                con.addRequestProperty("User-Agent", "CivicSage Document Reader");
+            }
+        };
     }
 
     private boolean areValid(List<Document> documents) {
